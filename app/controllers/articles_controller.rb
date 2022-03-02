77e14752aa03f70d5b1520340d41f7f9bddbed20
tatcn8 <96024645+tatcn8@users.noreply.github.com>
2022-03-02 15:24:56 -0600

    comments/articles both working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret",
+except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -34,16 +38,14 @@ class ArticlesController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
-
-  def destroy
+def destroy
     @article = Article.find(params[:id])
     @article.destroy
 
     redirect_to root_path, status: :see_other
   end
-
   private
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
+  def article_params
+    params.require(:article).permit(:title, :body, :status)
+  end
 end
